94cc8c96f98415f8bf12b8e437505de9d1b06920
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2021-05-03 14:01:48 +0200

    list 4

---
+++ list4/MisraGries.scala
@@ -0,0 +1,78 @@
+import scala.collection.mutable.Map
+import scala.util.Random
+
+class Car(val brand: String) {}
+
+class Book(val title: String) {}
+
+class Employee(val name: String) {}
+
+object MisraGries {
+  def misraGries(
+      k: Int,
+      stream: LazyList[AnyRef]
+  ): Map[String, Map[String, Int]] = {
+    var maps: Map[String, Map[String, Int]] = Map()
+
+    stream.foreach(element => {
+      val className = element.getClass().getSimpleName()
+
+      val map = maps.get(className) match {
+        case None => {
+          val newMap: Map[String, Int] = Map()
+          maps = maps + (className -> newMap)
+          newMap
+        }
+        case Some(value) => value
+      }
+
+      def matchCase(key: String) = map.get(key) match {
+        case Some(value) => map(key) = value + 1
+        case None => {
+          if (map.keySet.size < k - 1) { map(key) = 1 }
+          else {
+            map.keySet.foreach(key => {
+              map(key) = map(key) - 1
+              if (map(key) == 0) map.remove(key)
+            })
+          }
+        }
+      }
+
+      element match {
+        case element: Car      => matchCase(element.brand)
+        case element: Book     => matchCase(element.title)
+        case element: Employee => matchCase(element.name)
+      }
+    })
+
+    maps
+  }
+
+  def main(args: Array[String]) {
+    val inputStream: LazyList[AnyRef] = Random
+      .shuffle(
+        List[AnyRef](
+          new Car("BMW"),
+          new Car("Mercedes"),
+          new Car("Mercedes"),
+          new Car("Mercedes"),
+          new Car("BMW"),
+          new Car("BMW"),
+          new Car("BMW"),
+          new Car("BMW"),
+          new Car("Opel"),
+          new Car("Opel"),
+          new Car("Opel"),
+          new Employee("Nowak"),
+          new Employee("Nowak"),
+          new Book("Cool Book"),
+          new Book("Uncool Book"),
+          new Book("Cool Book")
+        )
+      )
+      .to(LazyList)
+
+    print(misraGries(3, inputStream))
+  }
+}
